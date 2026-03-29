dc0920cd91ecd83a6a54cd86a24a43b48dfb5360
Hitansh Doshi <41910867+hitansh16@users.noreply.github.com>
2026-03-29 10:17:01 -0400

    Initial commit

---
+++ src/main/include/Constants.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace ShooterConstants {
+constexpr int kBottomRightShooterID = 23;  // X60 - CANivore
+constexpr int kTopRightShooterID = 24;     // X60 - CANivore
+constexpr int kBottomLeftShooterID = 33;   // X60 - CANivore
+constexpr int kTopLeftShooterID = 34;      // X60 - CANivore
+constexpr int kHoodID = 54;                // Vortex - RIO
+
+constexpr const char* kCANivoreName = "Chassis";
+}  // namespace ShooterConstants
+
+namespace FeederConstants {
+constexpr int kFeederID = 50;    // Vortex - RIO
+constexpr int kConveyorID = 51;  // Vortex - RIO
+}  // namespace FeederConstants
+
+namespace IntakeConstants {
+constexpr int kIntakeID = 52;        // Vortex - RIO
+constexpr int kIntakeDeployID = 53;  // Vortex - RIO
+}  // namespace IntakeConstants
